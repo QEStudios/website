57364e27a4d7b85ad58d8fc9d63b4f9ee198c648
SKM GEEK <38995820+QEStudios@users.noreply.github.com>
2023-02-28 04:24:32 +0000

    Add intro animation and fuzzy surprise animation

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 var bouncing = false;
-var fuzzy = document.getElementById('fuzzy');
+var fuzzy = document.querySelector('.fuzzy');
 fuzzy.addEventListener("transitionend", switchDirection, false);
 function fuzzyClick() {
 	bouncing = !bouncing;
 	console.log(bouncing);
 	if(bouncing) {
-		fuzzy.classList.add('bouncing', 'bounce', 'bounce-1');
+		fuzzy.classList.add('bouncing', 'bounce', 'bounce-start');
 	} else {
 		fuzzy.classList.remove('bouncing');
 	}
 }
 function switchDirection() {
-	if(fuzzy.classList.contains('bounce-1')) {
+	if(fuzzy.classList.contains('bounce-start')) {
+		fuzzy.classList.remove('bounce-start');
+		fuzzy.classList.add('bounce-2');
+	} else if(fuzzy.classList.contains('bounce-1')) {
 		fuzzy.classList.remove('bounce-1');
 		fuzzy.classList.add('bounce-2');
 	} else if(fuzzy.classList.contains('bounce-2')) {
@@ -19,7 +22,10 @@ function switchDirection() {
 		if(fuzzy.classList.contains('bouncing')) {
 			fuzzy.classList.add('bounce-1');
 		} else {
+			fuzzy.classList.add('bounce-end');
 			fuzzy.classList.remove('bounce');
 		}
+	} else if(fuzzy.classList.contains('bounce-end') && !document.querySelector(".fuzzy:hover")) {
+		fuzzy.classList.remove('bounce-end');
 	}
 }
